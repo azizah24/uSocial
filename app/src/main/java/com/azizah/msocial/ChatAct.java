f83f1f9689e5798216c9ca60f2ad49c76a7c7a1c
azizah <azizahif99@gmail.com>
2020-11-29 17:06:17 +0700

    penambahan fitur notifikasi postingan & fix NullPointerException di FirebaseMessaging

--- app/src/main/java/com/azizah/msocial/ChatAct.java
+++ app/src/main/java/com/azizah/msocial/ChatAct.java
@@ -735,7 +735,7 @@ public class ChatAct extends AppCompatActivity {
 
                                 Map<String, String> headers = new HashMap<>();
                                 headers.put("Content-Type", "application/json");
-                                headers.put("Authorization", "key=AAx");
+                                headers.put("Authorization", "key=AAAAImjs9vw:APA91bFnhPsORo2XbYqFyRbisa6SMOakNMjpnR6GFfeeaAxA_qn1o4J3BjggWnL5SnOsEDTPCaNuZXdHI4ZI70GMzo7HB_pHxvEkr0iWX4w_QefCpIz2O1J71X5oyhxEw5Ak8YSRTNFx");
                                 return headers;
                             }
                         };
